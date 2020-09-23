86d4c9dd1dcb8159670a1bc6381d7fdcf790978c
Clayton Siby <claytonsiby@gmail.com>
2020-09-24 00:36:50 +0200

    Update registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,19 +1,8 @@
 class RegistrationsController < Devise::RegistrationsController
   private
 
-<<<<<<< HEAD
-  private
-
   def sign_up_params
     params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
-  end
-
-  def account_update_params
-    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
-=======
-  def sign_up_params
-    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
->>>>>>> 60821e25c4593536f5ff479d56d54a7c92dffd06
   end
 
   def account_update_params
