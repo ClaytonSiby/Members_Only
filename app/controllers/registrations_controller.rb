60821e25c4593536f5ff479d56d54a7c92dffd06
rumbie <rumbiemudzie@gmail.com>
2020-09-24 00:30:10 +0200

    Assign permittions to user in registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,19 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
+  private
 
-  before_action :configure_permitted_parameters
-
-  protected
-
-  # my custom fields are :name, :heard_how
-  def configure_permitted_parameters
-    devise_parameter_sanitizer do |u| 
-      u.permit(:sign_up, keys: [:name, :username,
-        :email, :password, :password_confirmation])
-    end
-    devise_parameter_sanitizer do |u| 
-      u.permit(:account_update, keys: [:name, :username,
-        :email, :password, :password_confirmation, :current_password])
-    end
+  def sign_up_params
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
   end
 
+  def account_update_params
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
+  end
 end
